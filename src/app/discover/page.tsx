fb4f85b17d3f4c6630c5ea9fc276c4fae105372d
Emeka <emekaamaechi0@gmail.com>
2024-12-24 18:17:32 +0100

    fixed build errors

--- src/app/discover/page.tsx
+++ src/app/discover/page.tsx
@@ -32,6 +32,10 @@ const Discover = () => {
     console.log(`Fetching data for page ${page}`)
   }
   return (
+    <div>
+
+    {
+      isAuthenticated && (
     
         <MainLayout>
     <section className="bg-background px-4 lg:px-16">
@@ -140,7 +144,9 @@ const Discover = () => {
        <div className="w-48 my-4"> <Button text="Explore Book Clubs" color="signup"/></div>
       </div>
         </MainLayout>
-    
+      )
+}
+        </div>
   )
 }
 
